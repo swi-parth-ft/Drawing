3e6aee1660a3282f6d7bdb69a8a9e87b77ea07bc
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2023-02-13 14:59:28 -0500

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Parth Antala on 2023-02-13.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
